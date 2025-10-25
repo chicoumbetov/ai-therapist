c642588d79753d8320873285c33fa26e4bf58dc5
chicoumbetov <shyngysmaaan@gmail.com>
2025-10-25 16:00:45 +0200

    :sparkles: backend db connect to mongoose

---
+++ backend/src/utils/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { logger } from "./logger";
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || // * REAL mongodb is hidden and imported in deployment server as env variable
+  "mongodb+srv://blocklinklabs:rKjw3YwImNAzgMzU@cluster0.znbgmwt.mongodb.net/ai-therapy";
+  // open mongo db
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    logger.info("Connected to MongoDB Atlas");
+  } catch (error) {
+    logger.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
